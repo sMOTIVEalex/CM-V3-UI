bb21173a80171c0c7bc4329458c54b5ef8458d22
Alex <abagithub@smotive.de>
2024-08-12 21:13:34 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
 
 export const network = (process.env.NEXT_PUBLIC_SOLANA_NETWORK ||
   WalletAdapterNetwork.Devnet) as WalletAdapterNetwork;
-const network = WalletAdapterNetwork.Devnet;
+// const network = WalletAdapterNetwork.Devnet;
 export const rpcHost =
   process.env.NEXT_PUBLIC_RPC_HOST || clusterApiUrl(network);
 
