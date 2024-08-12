f7448662d9de9fdd609be6de690fb98a648a38db
Alex <abagithub@smotive.de>
2024-08-12 21:09:37 +0200

    Update config.ts
    Test NFT Settings

--- src/config.ts
+++ src/config.ts
@@ -3,13 +3,13 @@ import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
 
 export const network = (process.env.NEXT_PUBLIC_SOLANA_NETWORK ||
   WalletAdapterNetwork.Devnet) as WalletAdapterNetwork;
-// const network = WalletAdapterNetwork.Devnet;
+const network = WalletAdapterNetwork.Devnet;
 export const rpcHost =
   process.env.NEXT_PUBLIC_RPC_HOST || clusterApiUrl(network);
 
 export const candyMachineId = new PublicKey(
   process.env.NEXT_PUBLIC_CANDY_MACHINE_ID ||
-    "3zwFR3spiwbSSMtvVKG2bRT6ttqFoC3MHCafGP8ZrdLz"
+    "32d8ocYHFcM4JKoqC4PvGJt5z22w9ZEu176XuFDTMufm"
 );
 export const defaultGuardGroup =
   process.env.NEXT_PUBLIC_DEFAULT_GUARD_GROUP || undefined; // undefined means default
@@ -19,10 +19,5 @@ export const defaultGuardGroup =
 // "DAA8yRLu7acVs3kxaTyCjoEjNWGinLaCKVhDY29ASNua"
 
 export const whitelistedWallets = [
-  "53VVFtLzzi3nL2p1QF591PAB8rbcbsirYepwUphtHU9Q",
-  "85gHs1pKkZcNCDEbcfxqhfFNPWCsNFqPTMMbTQARspWy",
-  "FXoth7ba7LALmJGFaidCcMnhsxEagBDBDKCmkpr2QuDr",
-  "232Z5QNvQ4wRyraGWFpC5i3HEbqozEWgBCV95eWASaG1",
-  "D8ivzpXkG66VknRdVjEF1HjqS3T1tzcqraVR6FYaeLsV",
-  "sTAKERL4U8fbn2nUte6rPVdcrz2z3fEh1adQEDrGULr",
+  "3CL583oF6L3Td52tKTtN2e1XEU6tdA5V8YeDUWWNMG7c"
 ];
